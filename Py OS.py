29b72e68854a056d53fd27b7a214286770585445
HuaJi_King <3550514081@qq.com>
2021-08-03 11:16:51 +0800

    Dev 0.0.2

--- Py OS.py
+++ Py OS.py
@@ -1,22 +1,31 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 #加载模块
 import random
 import time
 
+#声明变量
+global passwordpadd, password, passwordadd, power_on
+
 #自定义函数
-def 
-#开始进入Python的世界
-global passwordpadd, password, passwordadd, power_on_time, power_on
-print(('启动时间：' + time.ctime()))
-power_on = 'False'
-if (power_on == 'True'):
-    power_on = random.randint(5, 30)
-    print((time.ctime() + ('本次启动时长' + power_on_time)))
-else:
-    print('第一次启动，设置新用户Administrator')
-    passwordadd = input('请输入密码')
-    passwordpadd = input('请输入密码')
-    if (passwordadd == passwordpadd):
-        password = input()
-        print((time.ctime() + ('密码设置成功，请记好您的密码：' + passwordadd)))
+def power(inputpasswords):
+    fo = open("password.txt","r+") 
+    password = fo.read(4)
+    print(password)
+    strs = str(password)
+    awa = len(strs)
+    print(awa)
+    if awa == 4:
+        if inputpasswords == password:
+            print(time.ctime()+" [ 控制台 ] "+'本次启动时长1秒')
     else:
-        pass
+        print(time.ctime()+" [ 控制台 ] "+'第一次启动，设置新用户')
+        fo.write(inputpasswords)
+        print(time.ctime()+" [ 控制台 ] "+"密码设置成功")
+
+#开始进入Python的世界
+print(time.ctime()+" [ 控制台 ] "+"Python OS Dev 0.0.2")
+print(time.ctime()+" [ 控制台 ] "+"开机")
+inputpassword = input(time.ctime()+" [ 控制台 ] "+"请输入4位数密码:")
+inputpassword = str(inputpassword)
+power(inputpassword)
