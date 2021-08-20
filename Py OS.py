914a15cef9d84eb516082fd12f7bd7618cbb9f8d
HuaJi_King <3550514081@qq.com>
2021-08-20 16:14:19 +0800

    Python OS Dev 0.1.0

--- Py OS.py
+++ Py OS.py
@@ -4,8 +4,9 @@
 import module
 
 #Welcome to PyOS world
-module.out("Python OS Dev 0.0.5")
+module.out("Python OS Dev 0.1.0")
 module.out("开机")
 module.out("请输入密码")
 inputpassword = input()
 module.login(inputpassword)
+module.menu(module.menulist())
--- module.py
+++ module.py
@@ -1,12 +1,23 @@
 import time
-import module
+import webbrowser
+
 from easygui import *
+
+import module
+
+
 #声明变量
 #global passwordpadd, password, passwordadd, power_on
+
+#系统项模块
 def out(message):
    print(time.ctime()+" [ 控制台 ] "+message)
    return
 
+def exit(message): 
+   msgbox(msg='message', title='Python OS', ok_button='终止程序')
+
+#功能项模块
 def login(password):
    if password == "123456":
       module.out("登录成功")
@@ -16,5 +27,27 @@ def login(password):
       exit()
    return
 
-def exit(message):
-   msgbox(msg='message', title='Py OS 退出页面', ok_button='终止程序')
+def search(message):
+   webbrowser.open("https://www.baidu.com/s?ie=utf-8&word="+message)
+
+def menulist():
+   act = {"1.百度一下"}
+   #打印act存储的菜单列表
+   msg = "菜单："+str(act)
+   module.out(msg)
+   message = input("请选择程序（错误则返回）：")
+   return(message)
+
+def menu(message):
+   if message == "1":
+      while True:
+         act = input("请输入要问的问题，exit退出")
+         if act != "exit":
+            module.search(act)
+         else:
+            exit(0)
+      
+   elif message == 'exit':
+      break
+   else:
+      module.menulist()
